eb24a6e0ab1559cd48b50c834c8e6b4593586df5
weiluntong <weiluntong.inbox@gmail.com>
2021-07-25 01:08:34 -0600

    Creating test

--- tests/test_protonvpn_server_load.py
+++ tests/test_protonvpn_server_load.py
@@ -1,5 +1,23 @@
-from protonvpn_server_load import __version__
+#!/usr/bin/env python3
 
+import argparse
+import protonvpn_server_load as vpn
+
+
+def input(args):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-l', '--location', type=str, nargs='*', default=[], help='The Country Code of the VPN Location')
+	parser.add_argument('-t', '--tiers', type=int, nargs='*', default=[], help='The tiers to filter on')
+
+	return vars(parser.parse_args(args))
+
+def test_protonvpn_server_load(capsys):
+	inputDict = input(['-t', '0', '-l', 'US'])
+	bestServers = vpn.find_a_server_in(inputDict['location'], inputDict['tiers'])
+	with capsys.disabled():
+		print(*map(lambda server: server['Name'] + ": " + str(server['Load']) + "%", bestServers),sep='\n')
+	assert len(bestServers)
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert vpn.__version__ == '0.1.0'
+
